0dcbaf104320d4e1885fcbfc4e66d92d8dd3489b
S. Nithin Utham <91834702+BadCyborg@users.noreply.github.com>
2024-02-26 12:54:35 +0530

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -11,12 +11,12 @@ module.exports = {
 const express = require('express');
 const router = express.Router();
 
-router.get('/fe',(req,res,next) => {
+router.get('/',(req,res,next) => {
 
 	res.send('Hello from Node Js using Nodemon');
 });
 
-router.get('/home', (req, res, next) =>{
+router.get('/admin', (req, res, next) =>{
 
 	res.render('home',null);
 });
